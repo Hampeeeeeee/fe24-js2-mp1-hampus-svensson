bc1807d8bdc73d503bc2e6a8bf8194165143071d
Hampeeeeeee <hampus_svensson99@hotmail.com>
2025-01-18 12:30:43 +0100

    Session 2.
    Changed the functionality in Product.js so that stock is no longer private and tied to one object instance.
    
    Did a lot of styling as well.

--- modules/Product.js
+++ modules/Product.js
@@ -1,7 +1,7 @@
 export class Product {
     #title;
     #imageUrl;
-    #stock;
+    stock;
     #price;
     #discountPercentage;
     #category;
@@ -10,7 +10,7 @@ export class Product {
     constructor(title, imageUrl, stock, price, discountPercentage, category, rating) {
         this.#title = title;
         this.#imageUrl = imageUrl;
-        this.#stock = stock;
+        this.stock = stock;
         this.#price = price;
         this.#discountPercentage = discountPercentage;
         this.#category = category;
@@ -18,19 +18,19 @@ export class Product {
     }
 
     displayInfo() {
-        console.log(`Name: ${this.#title} - ${this.#imageUrl} - Amount: ${this.#stock} - Price: $${this.#price} - 
+        console.log(`Name: ${this.#title} - ${this.#imageUrl} - Amount: ${this.stock} - Price: $${this.#price} - 
                     Discount: ${this.#discountPercentage}% - Category: ${this.#category} - Rating: ${this.#rating}`);
     }
 
     discountedPrice() {
-        return this.#price * (1 - this.#discountPercentage / 100);
+        return (this.#price * (1 - this.#discountPercentage / 100)).toFixed(2);
     }
 
     updatePurse() {
-        this.#stock -= 1;
+        this.stock -= 1;
 
-        if (this.#stock <= 0) {
-            this.#stock = 0;
+        if (this.stock <= 0) {
+            this.stock = 0;
         }
     }
 
@@ -40,28 +40,27 @@ export class Product {
 
         // Skapa ett namn för produkten
         const title = document.createElement('h3');
-        title.textContent = this.#title;
-        productCard.appendChild(title);
+        title.innerText = this.#title;
 
         const img = document.createElement('img');
         img.src = this.#imageUrl;
         img.alt = `Image of ${this.#title}`;
-        productCard.appendChild(img);
-
-        // Skapa en kort beskrivning
+        
+        // Skapa ett lagersaldo
         const stock = document.createElement('p');
-        stock.textContent = `Stock: ${this.#stock}`;
-        productCard.appendChild(stock);
-
+        stock.innerText = `Stock: ${this.stock}`;
+        
         // Skapa ett pris för produkten
         const discountedPrice = document.createElement('p');
-        discountedPrice.textContent = `Price: $${this.discountedPrice()}`;
-        productCard.appendChild(discountedPrice);
-
+        discountedPrice.innerText = `Price: $${this.discountedPrice()}`;
+        
         const addToCartBtn = document.createElement('button');
         addToCartBtn.innerText = "Add to Cart";
-        addToCartBtn.addEventListener('click', this.updatePurse);
-        productCard.appendChild(addToCartBtn);
+        addToCartBtn.addEventListener('click', () => {
+            this.updatePurse();
+            stock.innerText = `Stock: ${this.stock}`;
+        });
+        productCard.append(title, img, stock, discountedPrice, addToCartBtn);
     
         // Returnera kortet så det kan läggas till i DOM senare
         return productCard;
